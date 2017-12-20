b0a9447c6f94413757c319c06493a03a4438f17f
zwz <zwz@servyou.local>
2017-12-20 08:46:46 +0800

    spring-boot-data start

---
+++ spring-boot-data-mongodb/src/main/java/com/ximo/springbootdatamongodb/SpringBootDataMongodbApplication.java
@@ -0,0 +1,16 @@
+package com.ximo.springbootdatamongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 朱文赵
+ * @date 2017/12/19 19:30
+ */
+@SpringBootApplication
+public class SpringBootDataMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataMongodbApplication.class, args);
+	}
+}
---
+++ spring-boot-data-redis/src/main/java/com/ximo/redis/springbootdataredis/SpringBootDataRedisApplication.java
@@ -0,0 +1,12 @@
+package com.ximo.redis.springbootdataredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDataRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDataRedisApplication.class, args);
+	}
+}
